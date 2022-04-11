b5a12ba04e46c9a0d193313b3dc1d8a6930a62ec
awatersny <awatersny@gmail.com>
2022-04-11 15:39:28 -0400

    Add TimeCard model

--- myapp/models.py
+++ myapp/models.py
@@ -18,6 +18,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    cards = db.relationship('TimeCard', backref='employee', lazy=True)
 
     def __init__(self, email, username, password):
         self.email = email
@@ -30,4 +31,19 @@ class User(db.Model, UserMixin):
     
     def __repr__(self):
         return f"Username {self.username}"
+
+class TimeCard(db.Model):
+    __tablename__ = 'time_cards'
+    id = db.Column(db.Integer, primary_key=True)
+    time_in = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    time_out = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f"Card ID: {self.id} -- In: {self.time_in} -- Out: {self.time_out}"
+
+
 
